d18452632163ba745500d8417b0d70b125acf87c
OlegKorzun <OKorzun@ru.netnucleus.com>
2026-03-05 21:05:53 +0300

    Dial;

---
+++ dial/dial.php
@@ -0,0 +1,29 @@
+<?php
+
+if (!array_key_exists('h', $_REQUEST) || !array_key_exists('m', $_REQUEST)) {
+    exit(1);
+}
+
+$h = intval($_REQUEST['h']);
+$m = intval($_REQUEST['m']);
+
+$minute_angle = $m*6;
+$hour_angle = $h*30 + (($m*30) / 60);
+
+$dom = new DOMDocument();
+$dom->validateOnParse = true;
+$dom->loadXML(file_get_contents('0900.svg'));
+
+$xpath = new DOMXPath($dom);
+$xpath->registerNamespace('svg', 'http://www.w3.org/2000/svg');
+$he = $xpath->query("//*[@id='h']")->item(0);
+$me = $xpath->query("//*[@id='m']")->item(0);
+
+$me->setAttribute('transform', "rotate($minute_angle 100 100)");
+$he->setAttribute('transform', "rotate($hour_angle 100 100)");
+
+$svgs = $dom->getElementsByTagName('svg');
+
+header('Content-Type: image/svg+xml');
+header('Cache-Control: no-cache');
+print $dom->saveXML($svgs[0]);
---
+++ dial/random.php
@@ -0,0 +1,44 @@
+<?php
+
+$h = rand(0, 11);
+$m = rand(0, 59);
+$style = 'white';
+$user_answer = '';
+$right_count = 0;
+
+
+if (array_key_exists('user_answer', $_REQUEST)
+    && array_key_exists('right_answer', $_REQUEST))
+{
+    if ($_REQUEST['user_answer'] == $_REQUEST['right_answer']) {
+        $style = 'lightgreen';
+        $user_answer = '';
+        $right_count = intval($_REQUEST['right_count']) + 1;
+    } else {
+        $style = 'lightcoral';
+        $user_answer = $_REQUEST['user_answer'];
+        $right_count = intval($_REQUEST['right_count']);
+        if (preg_match('/(\d{2}):(\d{2})/', $_REQUEST['right_answer'], $matches)) {
+            $h = $matches[1];
+            $m = $matches[2];
+        }
+    }
+}
+
+$url = 'http://192.168.11.143/dial/dial.php?h='.$h.'&m='.$m;
+$right_answer = sprintf('%02d:%02d', $h, $m);
+
+?>
+<html>
+    <body style="background-color: <?php echo $style; ?>;">
+        <img src="<?php echo $url; ?>" />
+        <form>
+            <input type="hidden" name="right_answer" value="<?php echo $right_answer; ?>" />
+            <input type="hidden" name="right_count" value="<?php echo $right_count; ?>" />
+            <input type="text" value="<?php echo $right_count; ?>" disabled />
+            <br/>
+            <input type="text" name="user_answer" value="<?php echo $user_answer; ?>" id="user_answer" autofocus /><br/>
+            <input type="submit" value="Отправить">
+        </form>
+    </body>
+</html>
